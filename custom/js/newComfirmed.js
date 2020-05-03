9df7f5aa837f894bfc72490d13fdb5d497c57d66
chris097 <chrisfidel.international@gmail.com>
2020-05-03 08:38:32 +0100

    add new  file newcomfirmed.js

---
+++ custom/js/newComfirmed.js
@@ -0,0 +1,18 @@
+let newComfirmCases = document.getElementById('newComfirmedCases')
+console.log(newComfirmCases)
+let url = "https://api.covid19api.com/summary"
+const loadJson = get => {
+    const option = {
+        method: 'Get',
+        body: JSON.stringify(get),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }
+    return fetch(url, option )
+    .then(res => res.json())
+    .then(res => (res.Global.NewConfirmed))
+    .then(res => (newComfirmCases.textContent = res))
+    .catch(error => console.error(`Error: ${error}`))
+}
+loadJson()
