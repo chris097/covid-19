d69e0597d926fbb1f1ebdfeed48e1e7eb8a3a29a
chris097 <chrisfidel.international@gmail.com>
2020-05-04 09:03:50 +0100

    updated newcomfirmedcase

--- custom/js/newComfirmed.js
+++ custom/js/newComfirmed.js
@@ -1,5 +1,8 @@
 let newComfirmCases = document.getElementById('newComfirmedCases')
-console.log(newComfirmCases)
+let recoveredCase = document.getElementById('recovered')
+let toatalDeath = document.getElementById('total-death')
+let ad = document.querySelector('.realtime-data-card-count-off');
+console.log(ad)
 let url = "https://api.covid19api.com/summary"
 const loadJson = get => {
     const option = {
@@ -11,8 +14,12 @@ const loadJson = get => {
     }
     return fetch(url, option )
     .then(res => res.json())
-    .then(res => (res.Global.TotalDeaths))
-    .then(res => (newComfirmCases.textContent = res))
+    .then(res => (result = res.Global))
+    .then(res => (
+        newComfirmCases.textContent = result.TotalConfirmed,
+        recoveredCase.textContent = result.TotalRecovered,
+        toatalDeath.textContent = result.TotalDeaths
+        ))
     .catch(error => console.error(`Error: ${error}`))
 }
 loadJson()
